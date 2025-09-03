b653bb87688e6df0e1bd01320ec9d30873acf155
ajcates.com <ajcates.com@gmail.com>
2025-09-03 22:54:42 +0000

    Changes before Firebase Studio auto-run

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ import './style.css';
 // Get your Gemini API key by:
 // - Selecting "Add Gemini API" in the "Firebase Studio" panel in the sidebar
 // - Or by visiting https://g.co/ai/idxGetGeminiKey
-let API_KEY = 'TODO';
+let API_KEY = 'AIzaSyCuN4kuveOw-MSfllya6GN9dVDhHgQaGeY';
 
 let form = document.querySelector('form');
 let promptInput = document.querySelector('input[name="prompt"]');
